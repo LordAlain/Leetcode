3528c887e11df3b2cb95ce35f38ab885ac49352e
akommi <adityakommi@gmail.com>
2024-12-24 00:46:49 -0500

    Time: 4 ms (26.51%), Space: 17.7 MB (6.86%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,9 +2,7 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         char = 0
 
-        for ch in s:
-            char ^= ord(ch)
-        for ch in t:
+        for ch in s+t:
             char ^= ord(ch)
         
         return chr(char)
